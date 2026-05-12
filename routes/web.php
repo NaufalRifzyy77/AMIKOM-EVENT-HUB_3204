c09380dc886c0d1d6f0d86cf597d7c5cfe0ea5bd
unknown <naufalrifzy@students.amikom.ac.id>
2026-05-12 07:52:17 +0700

    Tambah sistem CRUD lengkap untuk Manajemen Kategori

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('events', AdminEventController::class);
     Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
-    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::resource('categories', CategoryController::class);
     Route::resource('partners', PartnerController::class);
 });
 
